98322d04ed0df41298b530cf242156bd6ce96324
Abdulrahman Mohamad Aarabi <abma@moe.dk>
2021-05-20 12:55:09 +0200

    add navbar

---
+++ src/client/components/About/About.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <div>
+        peaking or writing about a source text contributes to deeper
+        comprehension than merely reading since both actions require intense
+        scrutiny and examination. Reading and then engaging in a meaningful and
+        fruitful discussion with others challenges us to extend our thinking,
+        arrive at new insights, and consider different perspectives. Writing
+        about what we read requires critical thinking and active engagement in
+        order to compose a coherent piece for a particular purpose and audience.
+        Therefore, learning experiences that include writing, speaking, and
+        listening tasks foster further introspection and analysis of text.
+      </div>
+      <div>
+
+        <a href="/">back</a>
+      </div>
+    </div>
+  );
+}
